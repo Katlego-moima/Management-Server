ff44dc084e804ac64a4a67362df4dc952d8c1f3c
Kat <moimakatlego@outlook.com>
2023-11-06 12:15:49 +0200

    setting up the server and routes

---
+++ Routes/adminRoute.js
@@ -0,0 +1,31 @@
+import express from "express";
+import con from "../utils/db.js";
+import jwt from "jsonwebtoken";
+
+const router = express.Router();
+
+router.post("/adminlogin", (req, res) => {
+  //matching user details with the details in the database
+
+  const sql = "SELECT * FROM admin WHERE email = ? AND password = ?";
+
+  con.query(sql, [req.body.email, req.body.password], (err, result) => {
+    if (err) return res.json({ loginStatus: false, Error: "Query error" });
+    if (result.length > 0) {
+      const email = result[0].email;
+      //generate a token
+      const token = jwt.sign(
+        { role: "admin", email: email },
+        "secret_for_katlego",
+        { expiresIn: "1d" }
+      );
+      res.cookie('token', token)
+      return res.json({ loginStatus: true});
+    }
+    else {
+        return res.json({ loginStatus: false, Error: "Wrong email or password" });
+    }
+  });
+});
+
+export { router as adminrouter };
--- index.js
+++ index.js
@@ -1,6 +1,16 @@
 import express from "express";
+import cors from 'cors';
+import {adminrouter} from './Routes/adminRoute.js'
+
 
 const app = express();
+app.use(cors({
+  origin: ['http://localhost:3000'],
+  methods: ['GET', 'POST', 'PUT'],
+  credentials: true
+}));
+app.use(express.json());
+app.use('/auth',adminrouter)
 
 const PORT = 3200;
 
